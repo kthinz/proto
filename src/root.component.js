8808800996b6146dd603aca4040030aa0191ba00
kthinz <128646068+kthinz@users.noreply.github.com>
2023-03-22 18:24:58 -0400

    Add files via upload

---
+++ src/root.component.js
@@ -0,0 +1,10 @@
+export default function Root(props) {
+  return (
+  <section>
+    {props.name} is mounted!
+    <br />
+    <a href="/angular">Go to angular app</a>
+    </section>
+  );
+  
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,15 @@
 const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-ts");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "app";
   const defaultConfig = singleSpaDefaults({
-    orgName,
-    projectName: "root-config",
+    orgName: "react-app",
+    projectName: "react-app",
     webpackConfigEnv,
     argv,
-    disableHtmlGeneration: true,
   });
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    plugins: [
-      new HtmlWebpackPlugin({
-        inject: false,
-        template: "src/index.ejs",
-        templateParameters: {
-          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
-          orgName,
-        },
-      }),
-    ],
   });
 };
